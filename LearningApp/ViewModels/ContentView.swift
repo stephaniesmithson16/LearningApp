0f72a4054a4aa705aea57e046a1a00c7a0163309
Stephanie Smithson <stephaniesmithson16@gmail.com>
2022-09-29 20:50:05 -0500

    Create project Add folders

---
+++ LearningApp/ViewModels/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  LearningApp
+//
+//  Created by Stephanie Smithson on 9/29/22.
+//
+
+import Foundation
+
+class ContentView: ObservableObject {
+    
+    init() {
+        
+    }
+    
+}
--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  LearningApp
 //
 //  Created by Stephanie Smithson on 9/29/22.
@@ -7,7 +7,10 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
+    
+    @EnvironmentObject var content:ContentView
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +19,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
