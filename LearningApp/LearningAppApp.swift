bfb61caf555b18aa63a197f5b458429b8c993559
Stephanie Smithson <stephaniesmithson16@gmail.com>
2022-09-29 20:44:45 -0500

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Stephanie Smithson on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
