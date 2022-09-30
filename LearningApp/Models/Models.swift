a66637352df0beacb393f307c7a4a04f588d7184
Stephanie Smithson <stephaniesmithson16@gmail.com>
2022-09-29 23:32:52 -0500

    Add data models

---
+++ LearningApp/Models/Models.swift
@@ -0,0 +1,55 @@
+//
+//  Models.swift
+//  LearningApp
+//
+//  Created by Stephanie Smithson on 9/29/22.
+//
+
+import Foundation
+
+class Module: Identifiable, Decodable {
+    
+    var id:Int
+    var category:String
+    var content:Content
+    var test:Test
+    
+}
+
+class Content: Identifiable, Decodable {
+    
+    var id:Int
+    var image:String
+    var time:String
+    var description:String
+    var lessons:[Lesson]
+    
+}
+
+class Lesson: Identifiable, Decodable {
+    
+    var id:Int
+    var title:String
+    var video:String
+    var duration:String
+    var explanation:String
+    
+}
+
+class Test: Identifiable, Decodable {
+    
+    var id:Int
+    var image:String
+    var time:String
+    var questions:[Question]
+    
+}
+
+class Question: Identifiable, Decodable {
+    
+    var id:Int
+    var content:String
+    var correctIndex:Int
+    var answers:[String]
+    
+}
