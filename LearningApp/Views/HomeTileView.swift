63598da3479581ab6db494357f4c8f3da2eac4c3
Stephanie Smithson <stephaniesmithson16@gmail.com>
2022-10-01 17:43:27 -0500

    Create Tile View

---
+++ LearningApp/Views/HomeTileView.swift
@@ -0,0 +1,62 @@
+//
+//  HomeTileView.swift
+//  LearningApp
+//
+//  Created by Stephanie Smithson on 10/1/22.
+//
+
+import SwiftUI
+
+struct HomeTileView: View {
+    var image:String
+    var title:String
+    var description:String
+    var count:String
+    var time:String
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .cornerRadius(10)
+                .foregroundColor(.white)
+                .aspectRatio(CGSize(width: 335, height: 175), contentMode: .fit)
+                .shadow(radius: 5)
+            
+            HStack(spacing: 20) {
+                Image(image)
+                    .resizable()
+                    .frame(width: 116, height: 116)
+                    .clipShape(Circle())
+                    
+                VStack(alignment: .leading) {
+                    Text(title)
+                        .font(.title)
+                        .bold()
+                        .padding(.top, 10)
+                    
+                    Text(description)
+                        .font(.callout)
+                        .padding(.bottom, 20)
+
+                    HStack {
+                        Image(systemName: "text.book.closed")
+                        Text(count)
+                            .font(.callout)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "clock")
+                        Text(time)
+                            .font(.callout)
+                    }.padding(.bottom, 10)
+                }
+            }.padding(.horizontal, 10)
+        }.padding()
+    }
+}
+
+struct HomeTileView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeTileView(image: "swift", title: "Learn Swift", description: "Understand the fundamentals of the Swift programming language.", count: "10 Lessons", time: "3 Hours")
+    }
+}
--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -19,6 +19,6 @@ struct HomeView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        HomeView().environmentObject(ContentModel())
     }
 }
